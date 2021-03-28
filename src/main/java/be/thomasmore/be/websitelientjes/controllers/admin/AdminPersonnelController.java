b1adda10c57736a1b596c2d8f4a974c065243f99
Dries Delanghe <delanghe.dries.1998@gmail.com>
2021-03-28 19:47:40 +0200

    added personeel page

--- src/main/java/be/thomasmore/be/websitelientjes/controllers/admin/AdminPersonnelController.java
+++ src/main/java/be/thomasmore/be/websitelientjes/controllers/admin/AdminPersonnelController.java
@@ -6,26 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @RequestMapping("/admin")
 @Controller
-public class AdminController {
+public class AdminPersonnelController {
 
     @Autowired
     PersonnelRepository personnelRepository;
 
-    @GetMapping({"/", "", "/home"})
-    public String home(Model model){
+    @ModelAttribute("personnelList")
+    public List<Personnel> getPersonnelList(){
+        return (List<Personnel>) personnelRepository.findAll();
+    }
 
-        List<Personnel> personnelList = (List<Personnel>) personnelRepository.findAll();
-        Integer listSize = personnelList.size();
-        Integer iterationSize = listSize/4;
+    @GetMapping("/personeellijst")
+    public String personeellijst(Model model){
 
-        model.addAttribute("iterationSize", iterationSize);
-        model.addAttribute("personnelList", personnelList);
-        return "admin/home";
+        return "admin/personeellijst";
     }
 }
