dd3b910086bb473efd4b7a4d60f59506106a8a31
Dries Delanghe <delanghe.dries.1998@gmail.com>
2021-04-30 15:19:06 +0200

    set up second filevalidation part to support 2 upload buttons on one page

--- src/main/resources/static/js/FileValidation.js
+++ src/main/resources/static/js/FileValidation.js
@@ -18,4 +18,24 @@ Filevalidation = () => {
             }
         }
     }
+
+    const fi1 = document.getElementById('formFile1');
+    const button1 = document.getElementById('fileUpload1');
+    const info1 = document.getElementById('size1');
+    if(fi1.files.length > 0){
+        for(let i = 0; i <= fi.files.length - 1; i++){
+            let fsize = fi.files.item(i).size;
+            let file = Math.round((fsize/1024));
+
+            if(file >= 2048){
+                button1.disabled = true;
+                info1.innerHTML = '<span class="text-danger"> File is <b>' + file + '</b> Kb groot, gelieve een file van minder dan ' +
+                    '2048Kb te selecteren. Ga naar <a target="_blank" href="https://compressimage.toolur.com/">deze site</a> als je de afbeelding wilt comprimeren</span>'
+            }
+            else{
+                button1.disabled = false;
+                info1.innerHTML = '<b>' + file + '</b> KB';
+            }
+        }
+    }
 }
